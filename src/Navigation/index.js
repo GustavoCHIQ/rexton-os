acf639e195621dacb7c01256b294cae06bea1a4b
Gustavo Chiquitin <luis.gustavo09@hotmail.com>
2023-01-09 22:19:10 -0400

    Delete arquivos desnecessários

--- src/Navigation/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import StackNavigation from './StackNavigation'
-import { NavigationContainer } from '@react-navigation/native'
-
-export default props => (
-    <NavigationContainer>
-        <StackNavigation />
-    </NavigationContainer>
-)
--- src/services/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({
-    baseURL: "http://52.67.177.136:3000"
-})
-
-export default Api;
